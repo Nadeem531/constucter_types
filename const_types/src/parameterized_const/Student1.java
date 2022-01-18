df056d63ef047e5f826a568ee6f1d7d0dbcda75c
nadeem shakeeb <snadeemshakeeb@gmail.com>
2022-01-19 00:45:57 +0530

    "First Commit"

---
+++ const_types/src/parameterized_const/Student1.java
@@ -0,0 +1,23 @@
+package parameterized_const;
+public class Student1 {  
+    int id;  
+    String name;  
+    //creating a parameterized constructor  
+    Student1(int i,String n){  
+    id = i;  
+    name = n;  
+    }  
+    //method to display the values  
+    void display(){System.out.println(id+" "+name);}  
+   
+    public static void main(String args[]){  
+    //creating objects and passing values  
+    Student1 s1 = new Student1(111,"nadeem");  
+    Student1 s2 = new Student1(222,"asim");  
+    //calling method to display the values of object  
+    s1.display();  
+    s2.display();  
+   }  
+
+
+}
